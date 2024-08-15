cb488d8dac0750b0953d2a9d01bbad0a1ec2e451
ShaikMohammedSufian <167082075+ShaikMohammedSufian@users.noreply.github.com>
2024-08-15 13:25:14 +0530

    first commit

---
+++ app01/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+def Sufi(request):
+    return HttpResponse("Sufi is good boy and he needs to experience life")
+def Manzoor(request):
+    return HttpResponse("Manzoor needs to stop smoking and focus on his physical health and mental health")
